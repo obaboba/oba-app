7c9e10c2555bdb81c492c01514e46b3ef8812b3e
Rosen Sharma <rosen.sharma@gmail.com>
2020-03-29 21:53:28 -0700

    ios

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,19 @@
+import UIKit
+import Flutter
+import Firebase
+//import GoogleSignIn
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+  // Use Firebase library to configure APIs
+    FirebaseApp.configure()
+
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
